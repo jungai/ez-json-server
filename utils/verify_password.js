f07c9f0dd8f247fe2f4fca2032ec460de7e2c232
jungai <juworaphol125@gmail.com>
2019-04-16 17:53:35 +0700

    remove comment

--- utils/verify_password.js
+++ utils/verify_password.js
@@ -1,8 +1,7 @@
 var bcrypt = require('bcryptjs')
-var hassPassword = require('./hash_password')
 
 function verifyPassword (rawPassword, hash) {
-  // var hash = hassPassword(rawPassword)
+
   return bcrypt.compareSync(rawPassword, hash)
 }
 
